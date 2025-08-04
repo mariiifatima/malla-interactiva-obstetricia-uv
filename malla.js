6a6caf60f987be53c32709b6ca3bccf14d0b83e3
mariiifatima <mariiifatima71@gmail.com>
2025-08-04 02:05:18 -0400

    Update malla.js

--- malla.js
+++ malla.js
@@ -1,1 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const ramos = document.querySelectorAll(".ramo");
+
+  ramos.forEach((ramo) => {
+    if (!ramo.classList.contains("bloqueado")) {
+      ramo.addEventListener("click", () => {
+        desbloquearRamos(ramo.dataset.id);
+      });
+    }
+  });
+});
+
+function desbloquearRamos(idAprobado) {
+  const bloqueados = document.querySelectorAll(`.ramo.bloqueado[data-prereq="${idAprobado}"]`);
+
+  bloqueados.forEach((ramo) => {
+    ramo.classList.remove("bloqueado");
+    ramo.style.textDecoration = "none";
+    ramo.style.color = "#111";
+    ramo.style.backgroundColor = "#60a5fa";
+    ramo.addEventListener("click", () => {
+      desbloquearRamos(ramo.dataset.id);
+    });
+  });
+}
 
